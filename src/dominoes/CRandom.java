d1b1480e6fba30e243b10e5b9543b17fbe00eca9
cwuferrets <37879745+cwuferrets@users.noreply.github.com>
2018-04-17 19:39:57 -0700

    Update CRandom.java

--- src/dominoes/CRandom.java
+++ src/dominoes/CRandom.java
@@ -5,10 +5,36 @@
  */
 package dominoes;
 
+import java.util.Random;
+import java.util.List;
+import java.util.Collections;
+
 /**
  * Determines random elements of the game, such as the order domino pieces will
  * be picked, who moves first,
  */
 public class CRandom {
+    private final Random rand;
     
+    /**
+     * Constructor
+     */
+    public CRandom() {
+        this.rand = new Random();
+    }
+    
+    /**
+     * Determines if player one is first by uniform distribution
+     * @return true if player one should go first
+     */
+    public boolean isPlayerOneFirst() {
+        return rand.nextBoolean();
+    }
+    /**
+     * Shuffles dominoes with uniform distribution
+     * @param list is a list of dominoes to be shuffled
+     */
+    public void shuffleDominoes(List list) {
+        Collections.shuffle(list);
+    }
 }
